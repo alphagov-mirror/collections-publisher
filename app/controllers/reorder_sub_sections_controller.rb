687f4ee5b264dffd50c00d03c08c4e6d67de4dd0
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-01-29 17:19:53 +0000

    Wrap reordering of sub-sections in transaction
    Wrap the reorder action in a transaction so that it can be rolled back
    if the call to publishing-api fails. This has the benefit of preserving
    the record, including the current position of the sub-section.

--- app/controllers/reorder_sub_sections_controller.rb
+++ app/controllers/reorder_sub_sections_controller.rb
@@ -7,10 +7,21 @@ class ReorderSubSectionsController < ApplicationController
   end
 
   def update
+    success = true
     current_positions
-    set_positions(submitted_positions)
-    if draft_updater.send
-      redirect_to coronavirus_page_path(coronavirus_page.slug), notice: "Sections were successfully reordered."
+
+    SubSection.transaction do
+      set_positions(submitted_positions)
+
+      unless draft_updater.send
+        success = false
+        raise ActiveRecord::Rollback
+      end
+    end
+
+    if success
+      message = "Sections were successfully reordered."
+      redirect_to coronavirus_page_path(coronavirus_page.slug), notice: message
     else
       message = "Sorry! Sections have not been reordered: #{draft_updater.errors.to_sentence}."
       redirect_to reorder_coronavirus_page_sub_sections_path(coronavirus_page.slug), alert: message
